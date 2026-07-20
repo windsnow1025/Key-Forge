0433e0a74545a9063232e12b4905c6c323970091
windsnow1025 <windsnow1025@gmail.com>
2026-07-19 23:08:57 -0700

    feat: vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,22 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Key Forge',
+        short_name: 'KeyForge',
+        description: 'Master key based SHA password derivation and AES text cipher',
+        theme_color: '#121212',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
 })
